728090a23f75c124f1aa67c0cbced51230454374
Thabang Lenonyana <thabanglenonyana@Thabangs-MacBook-Air.local>
2025-05-02 13:24:28 +0200

    fix: Removed some unused components and deprecated features

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './shared/footer/footer.component';
-import { HeaderComponent } from './shared/header/header.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet],
   template: `
 
     <router-outlet></router-outlet>
